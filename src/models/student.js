4c857dd41028211f55d82b7a6440ad73c56b5ef5
unknown <developer@lrsservices.in>
2024-03-04 18:49:50 +0530

    attendance update

--- src/models/student.js
+++ src/models/student.js
@@ -3,6 +3,11 @@ import bcrypt from "bcrypt";
 import jwt from "jsonwebtoken";
 
 const student = mongoose.Schema;
+const attendanceSchema = mongoose.Schema({
+  date: { type: Date },
+  day: { type: String },
+  status: { type: String },
+});
 const studentSchema = new student(
   {
     fullName: { type: String, required: [true, "Full name is required"] },
@@ -25,6 +30,9 @@ const studentSchema = new student(
     },
     fee: {
       type: Number,
+    },
+    attendance: {
+      type: [attendanceSchema],
     },
     password: {
       type: String,
