583d23199eccb231f98b17f0bb426ee1f684e89b
Ayush <ayush.yadav.100100@gmail.com>
2024-04-08 04:43:14 +0530

    error message fix

--- src/routes/root.js
+++ src/routes/root.js
@@ -5,18 +5,16 @@ import meetingRouter from "./meetingRoutes.js";
 import studentRouter from "./studentsRoutes.js";
 import userRoutes from "./userRoutes.js";
 import classRouter from "./classRoutes.js";
-import teacherRoutes from "./teacherRoutes.js";
 
 const router = express.Router();
 const path = "/api/v1";
 
 // Define routes for different endpoints
 router.use(`${path}/auth`, authRouter);
-router.use(`${path}/register`, userRoutes);
+router.use(`${path}/users`, userRoutes);
 router.use(`${path}/class`, classRouter);
 router.use(`${path}/batch`, batchRouter);
 router.use(`${path}/students`, studentRouter);
-router.use(`${path}/students`, teacherRoutes);
 router.use(`${path}/meetings`, meetingRouter);
 
 // Export the router
